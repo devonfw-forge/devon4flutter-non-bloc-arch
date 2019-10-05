cbdfee02b314003b385062ca1d7db66f709d3eb7
Fasust <sebastianf333@googlemail.com>
2019-10-05 13:59:03 +0200

    stats

--- tools/stats.dart
+++ tools/stats.dart
@@ -2,25 +2,34 @@ import 'dart:io';
 
 const refMarker = 'ref-';
 const lineMarker = '\n';
+const flutterMarker = 'flutter';
+const flutterDevMarker = '(Flutter Dev Team';
 const imgMarker = '/wiki//images/';
-const codeMarker = "Codesnippt ";
+const codeMarker = "*Codesnippt ";
 const tldrMarker = 'TLDR';
+const importantMarker = '⚠';
 const sectionMarker = "\n# ";
 
 main(List<String> arguments) async {
   File input = File(arguments[0]);
   String text = await input.readAsString();
 
-  print('Sections:\t' +
-      sectionMarker.allMatches(text).length.toString() +
-      '\nImages:\t\t' +
+  print('Number of Sections:\t' +
+      (sectionMarker.allMatches(text).length + 1).toString() +
+      '\nImages:\t\t\t' +
       imgMarker.allMatches(text).length.toString() +
-      '\nCode Snippets:\t' +
+      '\nCode Snippets:\t\t' +
       codeMarker.allMatches(text).length.toString() +
-      '\nTLDRs:\t\t' +
+      '\nTLDRs:\t\t\t' +
       tldrMarker.allMatches(text).length.toString() +
-      '\nLines:\t\t' +
+      '\n⚠ Importants:\t\t' +
+      importantMarker.allMatches(text).length.toString() +
+      '\nLine Count:\t\t' +
       lineMarker.allMatches(text).length.toString() +
-      '\nReferences:\t' +
+      '\nThe Word Flutter:\t' +
+      flutterMarker.allMatches(text.toLowerCase()).length.toString() +
+      '\nRefs to Flutter Devs:\t' +
+      flutterDevMarker.allMatches(text).length.toString() +
+      '\nReferences:\t\t' +
       refMarker.allMatches(text).length.toString());
 }
