3b2bd39b4507271165067461c47dda126a350a60
TugbaDalmaz <89070914+TugbaDalmaz@users.noreply.github.com>
2021-11-23 14:04:35 +0100

    employee dialog

---
+++ update_employee_dialog/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<shared_preferences_ios/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences_ios/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
